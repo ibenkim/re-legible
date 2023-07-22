c7fcb5c43ac6d8f3587f2ad1c76af32ba15d3d77
Ben Kim <decaground4338@gmail.com>
2023-07-22 10:20:28 -0700

    url configuration

--- testing/urls.py
+++ testing/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'testing'
 urlpatterns = [
-    path("", views.testing, name="testing")
+    path("", views.testing, name="testing"),
+    path("result", views.result, name="result"),
+    path("upload", views.upload, name="upload")
 ]
--- testing/views.py
+++ testing/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def testing(request):
     return render(request, "testing/index.html")
+
+def result(request):
+    return render(request, "testing/result.html")
+
+def upload(request):
+    return render(request, "testing/upload.html")
