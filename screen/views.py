5fadd28123c255e3265880ad4facb9a46b1cd468
Ben Kim <decaground4338@gmail.com>
2023-07-21 19:51:45 -0700

    bug fixing

--- screen/views.py
+++ screen/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "hello/index.html")
+    return render(request, "screen/index.html")
 
 def ben(request):
     return HttpResponse("Welcome, master.")
