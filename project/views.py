2697e18de529d29e44e3dfddbc0360c18eb8a8ba
Ben Kim <decaground4338@gmail.com>
2023-07-23 00:35:41 -0700

    pythons

--- project/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def project(request):
-    return render(request, "project/index.html")
--- testing/views.py
+++ testing/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from ocr import predict
+import os
 
 # Create your views here.
 def testing(request):
@@ -9,4 +13,16 @@ def result(request):
     return render(request, "testing/result.html")
 
 def upload(request):
-    return render(request, "testing/upload.html")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        print('uploaded url: ' + uploaded_file_url)
+        result = predict(uploaded_file_url[1:])
+        print('result: ' + str(result))
+        return render(request, 'testing/result.html', {
+            'uploaded_file_url': uploaded_file_url,
+            'predict_result': result
+        })
+    return render(request, 'testing/index.html')
